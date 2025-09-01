53e4b654e6c956a1b34059908b931330e0396390
David Kachroo <david.kachroo@gmail.com>
2025-08-31 22:24:30 -0400

    Update ETF_project.py

--- ETF_project.py
+++ ETF_project.py
@@ -3,7 +3,7 @@
 # Built on VS Code as a Streamlit web application
 # ======================================================================================================================================
 # Overview: This Python script provides a comprehensive tool for ETF investment portfolio management & advisory.
-# It's designed to be user-friendly with text, visualizations, formulas, and metrics to help make informed investment decisions.
+# It is designed to be user-friendly with text, visualizations, formulas, and metrics to help make informed investment decisions.
 # It employs Streamlit for the frontend interface, yfinance to retrieve real-time ticker data, and PyPortfolioOpt to optimize ETF portfolios.
 
 # --------------------------------------------------------------------------------------------------------------------------------------
